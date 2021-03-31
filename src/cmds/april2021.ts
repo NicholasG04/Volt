0b6479b1e5206e1ce240d66464c532097f76eb08
Nicholas G <hello@nicholasg.me>
2021-03-31 23:15:46 +0200

    Added lowdb

--- src/cmds/april2021.ts
+++ src/cmds/april2021.ts
@@ -1,7 +1,11 @@
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
+import low from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
 
 class April2020 extends Command {
+  db: low.LowdbSync<any>;
+
   constructor() {
     super('april2021', {
       aliases: ['april2021'],
@@ -15,6 +19,12 @@ class April2020 extends Command {
       clientPermissions: ['CHANGE_NICKNAME'],
       channel: 'guild',
     });
+
+    const adapter = new FileSync('storage/april2021.json');
+    this.db = low(adapter);
+
+    this.db.defaults({ servers: [] })
+      .write();
   }
 
   // @ts-ignore
@@ -27,7 +37,9 @@ class April2020 extends Command {
     if (!args.action) return message.util!.reply('Please specify an action. Valid actions are `start`, `pause` and `revert`');
     switch (args.action) {
       case 'start':
-        return message.util!.reply('Powering up April Fools 2020');
+        message.util!.reply('Powering up April Fools 2020');
+
+        return message.util!.reply('April Fools 2020 is successfully initialised');
       case 'pause':
         return message.util!.reply('Pausing changing nicknames but NOT reverting old ones');
       case 'revert':
